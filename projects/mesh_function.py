62290320fed156f64f92a5fe95ee75542ddada34
Tarjei Heggset <tarjeihe@pinga.uio.no>
2024-09-13 16:21:20 +0200

    message

--- projects/mesh_function.py
+++ projects/mesh_function.py
@@ -2,13 +2,13 @@ import numpy as np
 
 
 def mesh_function(f, t):
-    pass
+    return f
 
 def func(t):
-    pass
+    return np.exp(-t)
 
 def test_mesh_function():
     t = np.array([1, 2, 3, 4])
-    f = np.array([np.exp(-1), np.exp(-2), np.exp(-3), np.exp(-12)])
+    f = np.array([np.exp(-1), np.exp(-2), np.exp(-3), np.exp(-4)])
     fun = mesh_function(func, t)
     assert np.allclose(fun, f)
